29680bf5afdb77c22552117dae6178e9b0099fcd
DAFigueiredo <69812826+DAFigueiredo@users.noreply.github.com>
2020-08-20 12:04:24 +0100

    Update MainActivity.kt
    functions and objects

--- app/src/main/java/com/example/functionsandclasses/MainActivity.kt
+++ app/src/main/java/com/example/functionsandclasses/MainActivity.kt
@@ -7,5 +7,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test()
+
+        println(sum(5,9))
+
+
     }
+
+    fun test() {
+        val x = 5 * 4
+        println(x)
+    }
+
+    fun sum(a:Int, b:Int):Int {
+        return a + b
+    }
+
+
 }
